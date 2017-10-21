163508f12913f75ef505506aeb1aed0d719041ed
Justin Collins <justin@justincollins.com>
2017-10-22 00:50:21 +0100

    basic player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public float thrust;
+	public float yaw;
+
+	private Rigidbody2D rigidBody;
+	private float drag = 0.4f;
+
+	void Start () {
+		rigidBody = GetComponent<Rigidbody2D> ();
+	}
+
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		transform.Rotate(new Vector3(0, 0, -moveHorizontal * yaw * Time.deltaTime));
+
+		if (moveVertical != 0) {
+			rigidBody.drag = 0.0f;
+			rigidBody.AddForce(moveVertical * transform.up * thrust * Time.deltaTime);
+		} else {
+			rigidBody.drag = drag;
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# asteroids
+# Asteroids
 A silly game to help me learn Unity3d
+
+## Assets
+All assets provided by [Kenney Donation Pack](https://kenney.itch.io/kenney-donation).
